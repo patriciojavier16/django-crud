014d98c6c196fca65de915ea41e364ca408744b9
Jacob <jacobowiz.7@protonmail.com>
2024-02-15 20:54:03 -0500

    add intergration

--- students/views.py
+++ students/views.py
@@ -3,8 +3,14 @@ from .models import Students
 
 # Create your views here.
 def listStudents(request):
-    students = Students.objects.all()
-    return render(request, 'listStudent.html',{"students": students})
+    api_url = 'http://localhost:9090/students'
+    response = request.get(api_url)
+    if response.status_code == 200:
+        students = response.json()
+        return render(request, 'listStudent.html',{"students": students})
+    else:
+        return render(request, 'error.html', {'message': 'Error al obtener datos'})
+    
 
 def createStudent(request):
     student = Students(dni=request.POST['dni'],nombre=request.POST['nombre'],apellido=request.POST['apellido'],genero=request.POST['genero'])
